9c322373b2f9a17b9716a42c00ec8908e9df735b
Dan <danielmaina6@gmail.com>
2023-07-04 23:39:33 -0400

    init

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='THE HANDYMAN OF NIAGARA REGION' title='HOME IMPROVEMENT, MAINTENANCE, & REPAIR' />
+            <p>Over 100 different satisfiyed customers across the Greater Toronto Area and Niagara regions. Trust your home in the hands of an expert.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                GET QUOTE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
